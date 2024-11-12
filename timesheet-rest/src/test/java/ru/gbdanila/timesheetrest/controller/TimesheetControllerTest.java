de3fcaad774ed7aa667f3477889bcb10f9389bc7
danila nikitin <danila.n2015@yandex.ru>
2024-11-12 05:48:39 +0300

    move to module timesheet-rest

---
+++ timesheet-rest/src/test/java/ru/gbdanila/timesheetrest/controller/TimesheetControllerTest.java
@@ -0,0 +1,12 @@
+package ru.gbdanila.timesheetrest.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimesheetControllerTest {
+
+    @Test
+    void findAll() {
+    }
+}
